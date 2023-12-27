c011b5416485af51961e814024e23965224569c3
DiOnEasy <d11062002d@gmail.com>
2023-12-27 21:08:14 +0200

    html is done

---
+++ app/sidebar/sidebar.data.js
@@ -0,0 +1,10 @@
+const sidebarLinks = [
+  { linkUlr: "", linkText: "Dashboard" },
+  { linkUlr: "", linkText: "Product" },
+  { linkUlr: "", linkText: "Customers" },
+  { linkUlr: "", linkText: "Income" },
+  { linkUlr: "", linkText: "Promote" },
+  { linkUlr: "", linkText: "Help" }
+];
+
+module.exports = sidebarLinks
---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+let gulp = require("gulp"),
+  pug = require("gulp-pug");
+
+gulp.task("pug", function () {
+  return gulp
+    .src("app/*.pug")
+    .pipe(pug({ pretty: true }))
+    .pipe(gulp.dest("app"));
+});
+
+gulp.task("watch", function () {
+  gulp.watch("app/**/*.pug", gulp.parallel("pug"));
+  gulp.watch("app/**/*.css", gulp.parallel("pug"));
+  gulp.watch("app/**/*.js", gulp.parallel("pug"));
+});
+
+gulp.task("default", gulp.parallel("watch"));
